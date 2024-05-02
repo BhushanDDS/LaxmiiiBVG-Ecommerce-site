1f045c64687dbd7452cb9f133e67c373f2b62ba7
bhushan <a230141@gmail.com>
2024-05-02 19:32:04 +0530

    client

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h1 className="text-center"></h1>
+            <p className="text-center mt-3">
+                Made with ❤️ by TeamSugarFree
+            </p>
+        </div>
+    )
+}
+
+export default Footer
